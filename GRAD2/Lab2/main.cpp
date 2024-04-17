f6e9cceab347b9d24b6b3b5c5a83d7165644408d
ResQkiT <yaizkima@gmail.com>
2024-04-17 17:34:01 +0300

    added move constructor for LinhedList

--- GRAD2/Lab2/main.cpp
+++ GRAD2/Lab2/main.cpp
@@ -3,25 +3,21 @@
 #include "LinkedList.h"
 #include <vector>
 template<class T>
-void printArr(DynamicArray<T> & arr){
+void printArr(LinkedList<T> & arr){
 
-    for (int i = 0; i < arr.get_size(); i++)
+    for (; !arr.isEmpty();)
     {
-        std::cout << arr[i] << std:: endl;
+        std::cout << arr.back() << std:: endl;
+        arr.pop_back();
     }
 }
 int main()
 {
     int arr[5] = {1, 2, 3, 4, 5};
     LinkedList<int> Ll(5, arr);
-    LinkedList<int> A(Ll);
-    Ll.push_front(99);
-    Ll.push_back(-115);
-    Ll.push_front(-1213);
-    Ll.print();
-    A.print();
-    std::cout << Ll.getSize()<< std::endl;
-    
+    LinkedList<int> A = std::move(Ll);
+    printArr(A);    
+    printArr(Ll);
 
     return 0;
 }
